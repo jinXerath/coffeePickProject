ea47450e8fc60b59ffda4cdbd93dae391b523324
bengjoon <byeong960@naver.com>
2023-09-21 19:52:38 +0900

    32423

--- coffeePick/src/main/java/com/cp/admin/board/dao/BoardDAO.java
+++ coffeePick/src/main/java/com/cp/admin/board/dao/BoardDAO.java
@@ -9,6 +9,7 @@ import com.cp.admin.board.vo.BoardVO;
 
 @Mapper
 public interface BoardDAO {
+	//공지 게시판
 	public List<BoardVO> boardList(BoardVO bvo);
 	public int boardListCnt(BoardVO bvo);
 	public int boardInsert(BoardVO bvo);
@@ -16,5 +17,15 @@ public interface BoardDAO {
 	public int boardDelete(int board_no);
 	public int boardUpdate(BoardVO bvo);
 	public void checkDelete(List<String> list);
-
+	
+	
+	// 이벤트 게시판
+	public List<BoardVO> eventList(BoardVO bvo);
+	public int eventListCnt(BoardVO bvo);
+	public int eventInsert(BoardVO bvo) ;
+	public BoardVO eventDetail(BoardVO bvo);
+	public int eventDelete(BoardVO bvo) ;
+	public int eventUpdate(BoardVO bvo) ;
+	public BoardVO eventUpdateForm(BoardVO bvo);
+	public void eventCheckDelete(List<String> list) ;
 }
--- coffeePick/src/main/java/com/cp/admin/board/service/BoardService.java
+++ coffeePick/src/main/java/com/cp/admin/board/service/BoardService.java
@@ -6,6 +6,7 @@ import com.cp.admin.board.vo.BoardVO;
 
 
 public interface BoardService {
+	// 공지 게시판
 	public List<BoardVO> boardList(BoardVO bvo);
 	public int boardListCnt(BoardVO bvo);
 	public int boardInsert(BoardVO bvo) throws Exception;
@@ -14,4 +15,15 @@ public interface BoardService {
 	public int boardUpdate(BoardVO bvo) throws Exception;
 	public BoardVO updateForm(BoardVO bvo);
 	public void checkDelete(List<String> list) throws Exception;
+	
+	
+	// 이벤트 게시판
+	public List<BoardVO> eventList(BoardVO bvo);
+	public int eventListCnt(BoardVO bvo);
+	public int eventInsert(BoardVO bvo) throws Exception;
+	public BoardVO eventDetail(BoardVO bvo);
+	public int eventDelete(BoardVO bvo) throws Exception;
+	public int eventUpdate(BoardVO bvo) throws Exception;
+	public BoardVO eventUpdateForm(BoardVO bvo);
+	public void eventCheckDelete(List<String> list) throws Exception;
 }
